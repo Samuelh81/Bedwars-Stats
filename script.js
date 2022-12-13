e0d2fa5972e87648a511520e44bb9a01a0ee4f29
BuildTools <unconfigured@null.spigotmc.org>
2022-12-12 23:12:49 -0600

    added apikey.json and new backgrounds, updated css

---
+++ script.js
@@ -0,0 +1,43 @@
+//init
+import apikey from './apikey.json' assert {type: 'json'};
+//main function
+async function updateData() {
+    let jsonData;
+
+    const result = await fetch(getApiLink("6ce02c86a86f46318777b0be28646213"))
+
+    jsonData = await result.json();
+
+    console.log(jsonData);
+
+    //Write code for data processing here
+    console.log(getApiLink("6ce02c86a86f46318777b0be28646213"));
+
+    var playerName = jsonData.player.displayName
+
+    document.getElementById("main").innerHTML = playerName
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+//other functions
+function getApiLink(uuid) {
+    var apiKey = apikey.keys[0].key;
+    return "https://api.hypixel.net/player?key="+apiKey+"&uuid="+uuid;
+}
+document.getElementById("button").addEventListener('click', event => {updateData();});
+
